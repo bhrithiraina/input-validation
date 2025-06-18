fb6491b46b7662c0d540448341f000e30247ff92
bhrithi raina <bhrithiraina@gmail.com>
2025-06-18 17:10:10 +0530

    add all files

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+app.get("/health-checkup", function (req, res) {
+  const username = req.headers.username;
+  const password = req.headers.password;
+  const kidneyId = Number(req.query.kidneyId);
+
+  if (username !== "bhrithi" || password !== "pass") {
+    res.status(400).json({ msg: "Somethings up with your input" });
+  } else {
+
+    if (kidneyId !== 1 && kidneyId !== 2) {
+    res.status(400).json({ msg: "Somethings up with your input" });
+    } else{
+       // do something with kidney here 
+        res.json({
+            msg: "your kidney is fine"
+        })
+    }
+  }
+});
+
+app.listen(3001);
+
