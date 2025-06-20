d08cf90e67be940a1f4b722fb5f465eaf200fc18
bhrithi raina <bhrithiraina@gmail.com>
2025-06-20 14:08:47 +0530

    add all changes

--- zod.js
+++ zod.js
@@ -23,3 +23,25 @@
 
 // app.listen(3001);
 
+const zod = require("zod");
+
+// if this is an array of number with atleast 1 input, return true, else return false
+function validateInput(obj) {
+    const schema = zod.object({
+        email: zod.String().email();
+        password: zod.String().min(8);
+    })
+
+    const response = schema.safeParse(obj);
+    console.log(response);
+}
+
+app.post("/login", function (req, res) {
+    const response = validateInput(req.body)
+    if (!response.success) {
+        res.json({
+            msg: "your inputs are invalid"
+        })
+        return;
+    }
+})
