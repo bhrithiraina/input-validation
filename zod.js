03697995a78fef0bba575a5a32ef6c95d0dfd605
bhrithi raina <bhrithiraina@gmail.com>
2025-06-20 12:56:49 +0530

    add all changes

--- zod.js
+++ zod.js
@@ -10,9 +10,15 @@ app.post("/health-checkup", function(req, res) {
     // kidneys = [1, 2]
     const kidneys = req.body.kidneys;
     const response = schema.safeParse(kidneys);
-    res.send({
-        response
+    if (!response.success) {
+        res.status(411).json({
+            msg: "input is invalid"
+        })
+    } else {
+        res.send({
+     response
     })
+    }
 });
 
 app.listen(3001);
