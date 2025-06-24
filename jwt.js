aa1c521ea688cba21720095d3baba6e5ae2ac67a
bhrithi raina <bhrithiraina@gmail.com>
2025-06-24 12:41:57 +0530

    add all changes

--- jwt.js
+++ jwt.js
@@ -26,7 +26,7 @@ const ALL_USERS = [
 function userExists(username, password) {
   // write logic to return true or false if this user exists
   // in ALL_USERS array 
-  const userExists = false;
+  let userExists = false;
   for (let i = 0; i < ALL_USERS.length; i++) {
     if(ALL_USERS[i].username == username && ALL_USERS[i].password == password) {
       userExists = true;
