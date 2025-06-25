78f10e3b6b18d6c0a690b74cfd79261018835bf7
bhrithi raina <bhrithiraina@gmail.com>
2025-06-25 15:49:10 +0530

    add all chages

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const app = express();
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb+srv://bhrithiraina:bhrithi123@cluster0.qyfkbus.mongodb.net/userappnew?")
