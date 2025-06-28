3de06277b346701e807ac1418939f54ecc4f8887
bhrithi raina <bhrithiraina@gmail.com>
2025-06-28 12:58:30 +0530

    add all changes

--- index2.js
+++ index2.js
@@ -7,22 +7,26 @@ function isOldEnoughMiddleware(req, res, next) {
         next();
     } else {
         res.json({
-            msg: "Sorry you are not og age yet"
+            msg: "Sorry you are not of age yet"
         })
     }
 }
 
-app.get("/ride1", isOldEnoughMiddleware, function(req, res) {
+
+
+app.get("/ride1", function(req, res) {
         res.json({
         msg: "you have successfully riden the ride 1"
     })
 })
 
-app.get("/ride2", isOldEnoughMiddleware, function(req, res) {
+app.get("/ride2", function(req, res) {
         res.json({
         msg: "you have successfully riden the ride 2",
         })
 })
+
+app.use(isOldEnoughMiddleware);
 
 app.listen(3000); 
 
