22e42dc3edd5e6f4019dbffe7ad4290294292c23
bhrithi raina <bhrithiraina@gmail.com>
2025-06-28 12:50:05 +0530

    add all changes

--- index2.js
+++ index2.js
@@ -1,37 +1,27 @@
 const express = require("express");
 const app = express();
 
-// function that returns a boolean if the age of a person is more than 14
-function isOldEnough(age) {
+function isOldEnoughMiddleware(req, res, next) {
+    const age = req.query.age;
     if (age >= 14) {
-        return true;
+        next();
     } else {
-        return false;
+        res.json({
+            msg: "Sorry you are not og age yet"
+        })
     }
 }
 
-app.get("/ride1",  function(req, res) {
-    if (isOldEnough(req.query.age)) {
+app.get("/ride1", isOldEnoughMiddleware, function(req, res) {
         res.json({
         msg: "you have successfully riden the ride 1"
     })
-    } else {
-        res.status(411).json({
-            msg: "Sorry you are not of age yet"
-        })
-    }
 })
 
-app.get("/ride2",  function(req, res) {
-    if (isOldEnough(req.query.age)) {
+app.get("/ride2", isOldEnoughMiddleware, function(req, res) {
         res.json({
-        msg: "you have successfully riden the ride 2"
-    })
-    } else {
-        res.status(411).json({
-            msg: "Sorry you are not of age yet"
+        msg: "you have successfully riden the ride 2",
         })
-    }
 })
 
 app.listen(3000); 
